95df4e252c09838a187b400fee4de9a4767da0d1
leocavalcante <lc@leocavalcante.com>
2017-11-11 14:42:11 -0200

    Need help

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
-#![feature(plugin)]
+#![feature(plugin, custom_derive)]
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
 
-#[get("/hello/<name>")]
-fn index(name: String) -> String {
-    format!("Hello {}", name)
+use rocket::outcome::Outcome;
+use rocket::request::FromRequest;
+use rocket::request::Request;
+use rocket::request;
+use rocket::request::FromForm;
+
+#[derive(FromForm)]
+struct QueryString {
+    accessToken: String,
+}
+
+struct AccessToken(String);
+
+impl<'a, 'r> FromRequest<'a, 'r> for AccessToken {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<AccessToken, ()> {
+        let accessToken = // HELP!;
+        Outcome::Success(AccessToken(accessToken.to_string()))
+    }
+}
+
+#[get("/hello/<name>?<query_string>")]
+fn index(name: String, query_string: QueryString) -> String {
+    format!("Hello {} {}", name, query_string.accessToken)
 }
 
 fn main() {
