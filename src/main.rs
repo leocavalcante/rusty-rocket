79ea1a761a610f665ac21e7393346e69c8b75326
leocavalcante <lc@leocavalcante.com>
2017-11-11 17:30:33 -0200

    Struggling with lifetimes, borrows and cows!

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,52 @@
 #![feature(plugin, custom_derive)]
 #![plugin(rocket_codegen)]
 
-extern crate rocket;
+#[macro_use]
+extern crate log;
+extern crate simple_logger;
 
+extern crate rocket;
+extern crate url;
+
+use rocket::http::uri::URI;
 use rocket::outcome::Outcome;
+use rocket::request;
 use rocket::request::FromRequest;
 use rocket::request::Request;
-use rocket::request;
-use rocket::request::FromForm;
+use std::collections::HashMap;
+use url::form_urlencoded::Parse;
+use url::Url;
 
-#[derive(FromForm)]
-struct QueryString {
-    accessToken: String,
-}
+struct AccessToken<'r>(&'r str);
 
-struct AccessToken(String);
-
-impl<'a, 'r> FromRequest<'a, 'r> for AccessToken {
+impl<'a, 'r> FromRequest<'a, 'r> for AccessToken<'r> {
     type Error = ();
 
-    fn from_request(request: &'a Request<'r>) -> request::Outcome<AccessToken, ()> {
-        let accessToken = // HELP!;
-        Outcome::Success(AccessToken(accessToken.to_string()))
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<AccessToken<'r>, ()> {
+        let uri: &URI = request.uri();
+        let uri_str: &str = uri.as_str();
+        let url_str: String = format!("{}{}", "fakescheme:/", uri_str);
+        let maybe_url: Result<Url, _> = Url::parse(&url_str);
+
+        let url: Url = match maybe_url {
+            Ok(u) => u,
+            _ => panic!("Error parsing url"),
+        };
+
+        let pairs: Parse = url.query_pairs();
+        let query_hash: &'r HashMap<String, String> = pairs.into_owned().collect();
+        let access_token: &str = query_hash.get("access_token").unwrap();
+
+        Outcome::Success(AccessToken(access_token))
     }
 }
 
-#[get("/hello/<name>?<query_string>")]
-fn index(name: String, query_string: QueryString) -> String {
-    format!("Hello {} {}", name, query_string.accessToken)
+#[get("/hello/<name>")]
+fn index(access_token: AccessToken, name: String) -> String {
+    format!("Hello {} {}", name, access_token.0)
 }
 
 fn main() {
+    simple_logger::init().unwrap();
     rocket::ignite().mount("/", routes![index]).launch();
 }
